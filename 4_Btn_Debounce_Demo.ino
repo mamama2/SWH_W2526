e58c59189ef20a5bc0054cb25021c83741a71e02
Marcos <mamama2@gmail.com>
2025-11-11 15:46:06 +0100

    Add files via upload

---
+++ 4_Btn_Debounce_Demo.ino
@@ -0,0 +1,50 @@
+/*
+   Sketching mit Hardware
+  DMP - SWH - WS2526
+  - Debounce a pushbutton. Termin 5
+  This project demonstrates how to debounce a pushbutton and avoid unexpected results
+ BIEKER-WALZ / MARTINEZ
+
+
+#define BUTTON_PIN 2
+int buttonPress = 0;
+void setup() {
+  Serial.begin(9660);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  buttonPress = digitalRead(BUTTON_PIN);
+
+  if (buttonPress == 1) {
+    Serial.println("BANG!");
+  }
+}
+*/
+
+
+#define BUTTON_PIN 2
+int buttonPress = 0;
+
+unsigned long  lastTimeButtonChanged = millis();
+unsigned long  debounceDuration = 100; // millis()
+
+void setup() {
+  Serial.begin(9660);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (millis() - lastTimeButtonChanged > debounceDuration) {
+
+  
+    buttonPress = digitalRead(BUTTON_PIN);
+
+    if (buttonPress == 1) {
+      lastTimeButtonChanged = millis();
+      Serial.println("BANG!");
+      //delay(100);
+    }
+  }
+}
+
